c625ec5ed5c1d3281eaa31b868adb4f690e1546f
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-11-23 12:09:54 +0700

    Setup http server using axum

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use axum::{
+    error_handling::HandleErrorLayer, extract::Path, http::StatusCode, response::IntoResponse,
+    routing::post, Json, Router, Extension,
+};
+use axum_macros::debug_handler;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+use tokio::{sync::Notify, time::timeout};
+use tower::{BoxError, ServiceBuilder};
+use tower_http::trace::TraceLayer;
+
+type State = Arc<Mutex<HashMap<String, Arc<Notify>>>>;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let state = State::default();
+
+    // Compose the routes
+    let app = Router::new()
+        .route("/wait-for-second-party/:id", post(sync_point))
+        // Add middleware to all routes
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|error: BoxError| async move {
+                    if error.is::<tower::timeout::error::Elapsed>() {
+                        Ok(StatusCode::REQUEST_TIMEOUT)
+                    } else {
+                        Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {}", error),
+                        ))
+                    }
+                }))
+                .timeout(Duration::from_secs(30))
+                .layer(TraceLayer::new_for_http())
+                .into_inner(),
+        )
+        .layer(Extension(state));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
